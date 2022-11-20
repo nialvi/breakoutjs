23db907299d149787814b537455f8cf73c7cb9b0
Nikolaev Viktor <maclauddd@gmail.com>
2022-11-20 14:58:49 +0300

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+const webpack = require("webpack");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require("path");
+
+const config = {
+  entry: "./src/index.ts",
+  mode: "development",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts(x)?$/,
+        loader: "ts-loader",
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "src/index.html",
+    }),
+  ],
+};
+
+module.exports = config;
